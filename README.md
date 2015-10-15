2f07b7b0a7cddbcd6ab7bdd93777afa3d1db1c6b
jg <josegallegospdel@gmail.com>
2015-10-14 20:03:00 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+##Short glossary (sinicized)
+
+What started as an experiment to learn the workings of the Atom text editor became a project that I'll be updating over and over.
+an easy way to see this data is
+```
+console.table([array.json])
+```
+[]()
---
+++ defs.js
@@ -0,0 +1,100 @@
+[
+{
+  'englishTerm': 'Laptop Computer',
+  'chineseTerm': '本本',
+  'pinyin': 'benben',
+  'partOfSpeech': 'n.',
+  'usage': ' '
+},
+{
+  'englishTerm': 'iPhone',
+  'chineseTerm': '苹果手机',
+  'pinyin': 'pinguoshoudian',
+  'partOfSpeech': 'n.',
+  'usage': '  '
+},
+{
+  'englishTerm': 'directory',
+  'chineseTerm': '目录',
+  'pinyin': 'mulu',
+  'partOfSpeech': 'n.',
+  'usage': ' '
+},
+{
+  'englishTerm': 'repository',
+  'chineseTerm': '典藏',
+  'pinyin': 'diancang',
+  'partOfSpeech': 'n.',
+  'usage': '  '
+},
+{
+  'englishTerm': 'browser',
+  'chineseTerm': '浏览器',
+  'pinyin': 'liulanqi',
+  'partOfSpeech': 'n.',
+  'usage': ' '
+},
+{
+  'englishTerm': 'firefox',
+  'chineseTerm': '火狐',
+	'pinyin': 'huohu',
+  'partOfSpeech': 'n.',
+  'usage': ' '
+},
+{
+  'englishTerm': 'robot',
+  'chineseTerm': '机器人',
+	'pinyin': 'jiqiren',
+  'partOfSpeech': 'n.',
+  'usage': ' '
+},
+{
+  'englishTerm': 'electric car',
+  'chineseTerm': '电汽车',
+	'pinyin': 'dian qiche',
+  'partOfSpeech': 'n.',
+  'usage': ' '
+},
+{
+  'englishTerm': 'upload',
+  'chineseTerm': '上载',
+	'pinyin': 'shangzai',
+  'partOfSpeech': 'v.',
+  'usage': ' '
+},
+{
+  'englishTerm': 'download',
+  'chineseTerm': '下载',
+	'pinyin': 'xiazai',
+  'partOfSpeech': 'v.',
+  'usage': ' '
+},
+{
+  'englishTerm': 'gadget/widget',
+  'chineseTerm': '小玩意',
+	'pinyin': 'xiao wanyi',
+  'partOfSpeech': 'n.',
+  'usage': 'small software runnable '
+},
+{
+  'englishTerm': 'hard drive',
+  'chineseTerm': '硬碟',
+  'pinyin': 'yingdie',
+  'partOfSpeech': 'n.',
+   'usage': ' '
+},
+{
+  'englishTerm': 'plug-in',
+  'chineseTerm': '插进',
+	'pinyin': 'chajin',
+  'partOfSpeech': 'v.',
+  'usage': 'insert in the socket'
+},
+{
+  'englishTerm': 'plugIn',
+  'chineseTerm': '插件',
+  'pinyin': 'chajian',
+  'partOfSpeech': 'n.',
+  'usage': 'a software component'
+}
+]
